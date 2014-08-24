a75bc2cad7b1236f6811b99004bb5345b68e2c86
hmadrigal <hfmad@hotmail.com>
2014-08-24 11:48:58 -0600

    Added homework 2.2

--- homework_2_2/app.js
+++ homework_2_2/app.js
@@ -14,24 +14,27 @@ MongoClient.connect('mongodb://localhost:27017/weather',function(err,db) {
 			cursor : { batchSize : 1000}
 		});
 
-	//var foundDocIds = [];
+	var foundDocIds = [];
 	cursor.on('data',function(group){
-		//foundDocIds.push(group.documentId);
-		db.collection('data').findAndModify( { _id : group.documentId }, {}, {$set : { month_high : true } }, { 'new' : true }, function(err,doc) {
-			if (err) throw err;
-			console.log(doc);
-		});
-		console.log(group);
+		foundDocIds.push(group.documentId);
+		//db.collection('data').findAndModify( { _id : group.documentId }, {}, {$set : { month_high : true } }, { 'new' : true }, function(err,doc) {
+		//	if (err) throw err;
+		//	console.log(doc);
+		//});
+		//console.log(group);
 	});
 
 	cursor.on('end',function(){
 		//console.log(foundDocIds);
-		//foundDocIds.forEach(function(docId) {
-		//	db.collection('data').findAndModify( { _id : docId }, {}, {$set : { month_high : true} }, { 'new' : true }, function(err,doc) {
-		//		if (err) throw err;
-		//		console.log(doc);
-		//	});
-		//});
+		foundDocIds.forEach(function(docId) {
+			db.collection('data').findAndModify( { _id : docId }, {}, {$set : { month_high : true} }, { 'new' : true }, function(err,doc) {
+				if (err) throw err;
+				// NOTE: Workaround to detect when to close the connection. Checking if it's the last doc ID
+				if (foundDocIds[foundDocIds.length-1] == docId)
+					db.close();
+				console.log(doc);
+			});
+		});
 	});
 });
 
