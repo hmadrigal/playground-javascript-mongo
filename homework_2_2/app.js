4a8b5807411316d839ca17d22ed73f08a360d3c9
hmadrigal <hfmad@hotmail.com>
2014-08-24 11:09:18 -0600

    Adding some code for homework 2.2

---
+++ homework_2_2/app.js
@@ -0,0 +1,37 @@
+var 
+	MongoClient = require('mongodb').MongoClient
+;
+
+MongoClient.connect('mongodb://localhost:27017/weather',function(err,db) {
+	if (err) throw err;
+	
+	var cursor = db.collection('data')
+		.aggregate([
+			{ $sort : { Temperature : -1 } },
+			{ $group : { _id :  '$State',  highestTemperature : { $max : '$Temperature' }, documentId : { $first : '$_id'  } } }
+		], {
+			allowDiskUsage : true,
+			cursor : { batchSize : 1000}
+		});
+
+	//var foundDocIds = [];
+	cursor.on('data',function(group){
+		//foundDocIds.push(group.documentId);
+		db.collection('data').findAndModify( { _id : group.documentId }, {}, {$set : { month_high : true } }, { 'new' : true }, function(err,doc) {
+			if (err) throw err;
+			console.log(doc);
+		});
+		console.log(group);
+	});
+
+	cursor.on('end',function(){
+		//console.log(foundDocIds);
+		//foundDocIds.forEach(function(docId) {
+		//	db.collection('data').findAndModify( { _id : docId }, {}, {$set : { month_high : true} }, { 'new' : true }, function(err,doc) {
+		//		if (err) throw err;
+		//		console.log(doc);
+		//	});
+		//});
+	});
+});
+
